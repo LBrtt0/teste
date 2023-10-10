1432ba4dd63a442e1d045bfddbe52e324d4a72ff
LL <ana.lbpereira@aluno.uepa.br>
2023-10-10 10:32:19 -0300

    Definicao das variaveis

---
+++ Programa Coordenadas.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+
+//função que calcula distancia
+float calculaDistancia(int x1, int y1, int x2, int y2){
+	return sqrt(pow(x2-x1,2) + pow(y2-y1,,2));
+}
+
+//main
+
+int main(){
+
+	// definição das variáveis
+	int x1,x2,y1,y2;
+	float distancia;	
+}s
