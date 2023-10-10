7c88324704961c811fb830b000cc72a4827722a6
LL <ana.lbpereira@aluno.uepa.br>
2023-10-10 12:08:42 -0300

    Calculo e impressao juntas

--- distancia.c
+++ distancia.c
@@ -22,6 +22,46 @@ int main(){
 	
 	//calculo da distancia entre os pontos
 	distancia = calculaDistancia(x1,y1,x2,y2);
+	printf("Distancia: %f", distancia);		
+}
+}
+#include <stdio.h>
+#include <math.h>
+
+//função que calcula distancia
+float calculaDistancia(int x1, int y1, int x2, int y2){
+	return sqrt(pow(x2-x1,2) + pow(y2-y1,,2));
+}
+
+//main
+
+int main(){
+	
+}
+#include <stdio.h>
+#include <math.h>
+
+//função que calcula distancia
+float calculaDistancia(int x1, int y1, int x2, int y2){
+	return sqrt(pow(x2-x1,2) + pow(y2-y1,2));
+}
+
+//main
+
+int main(){
+	
+	//definição das variáveis
+	int x1,x2,y1,y2;
+	float distancia;
+	
+	//leitura das coordenadas dos pontos
+	printf("Informe as coordenadas para o primeiro ponto: ");
+	scanf("%i %i", &x1, &y1);
+	printf("Informe as coordenadas para o segundo ponto: ");
+	scanf("%i %i", &x2, &y2);
+	
+	//calculo da distancia entre os pontos
+	distancia = calculaDistancia(x1,y1,x2,y2);
 	
 	//mostrar o resultado
 	printf("Distancia: %f", distancia);		
